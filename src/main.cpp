913c3cf6de4032a5a26df578a489e36b801a8199
Stéphane ALBERT <stephane.albert.fr@gmail.com>
2020-08-04 08:22:13 +0200

    Fixed case in copyright notice.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 //
 // This file is part of racatoma.
 //
-// Racatoma is free software: you can redistribute it and/or modify
+// racatoma is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
-// Racatoma is distributed in the hope that it will be useful,
+// racatoma is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public
-// License along with Racatoma.  If not, see
+// License along with racatoma.  If not, see
 // <https://www.gnu.org/licenses/>.
 //
 
